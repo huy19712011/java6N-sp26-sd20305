3a144184e38a45ad6438925246e6766982a6457e
huy19712011 <35172019+huy19712011@users.noreply.github.com>
2026-03-26 15:11:46 +0700

    Security basics: customizing username and password using application.properties

--- src/main/java/org/example/java6nsp26sd20305/entity/Book.java
+++ src/main/java/org/example/java6nsp26sd20305/entity/Book.java
@@ -1,9 +1,6 @@
 package org.example.java6nsp26sd20305.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "books")
 public class Book {
 
     @Id
